cd9287a344012859a3309efec709d165dd051289
kjsb25 <kjsb25@mail.missouri.edu>
2016-04-26 01:24:13 -0500

    LoadingScreen

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class LoadOnClick : MonoBehaviour {
 
+    public GameObject loadingImage;
+
     public void LoadScene(int selection)
     {
         if (selection == 10)
         {
             Application.Quit();
         }
+        loadingImage.SetActive(true);
         Application.LoadLevel(selection);
     }
 }
