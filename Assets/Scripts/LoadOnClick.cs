589aa5896f19c7526b0c405a75be83a5de6bc190
kjsb25 <kjsb25@mail.missouri.edu>
2016-04-26 00:42:36 -0500

    QuickCommit

---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+    public void LoadScene(int selection)
+    {
+        if (selection == 10)
+        {
+            Application.Quit();
+        }
+        Application.LoadLevel(selection);
+    }
+}
